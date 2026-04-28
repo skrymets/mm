1570a95876a229cfbfc3febc6f5a1e83b794ccc0
Sergii Krymets <skrymets@gmail.com>
2026-04-28 23:03:52 +0300

    Remove obsolete XmlJavaTypeAdapters from parent actions package-info

--- src/main/java/freemind/controller/actions/package-info.java
+++ src/main/java/freemind/controller/actions/package-info.java
@@ -1,7 +1,1 @@
-@jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapters({
-    @jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter(
-        type = String.class,
-        value = freemind.common.StringEncoderAdapter.class
-    )
-})
 package freemind.controller.actions;
